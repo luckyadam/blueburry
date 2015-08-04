b67e91854f6657c8f112563f8ae6ba3c45ff3832
Simon Yu <ccl1115@gmail.com>
2015-08-04 19:42:28 +0800

    proxy server

--- proxy/server.js
+++ proxy/server.js
@@ -1,13 +1,17 @@
-var http = rquire('http');
+var http = require('http');
 var httpProxy = require('http-proxy');
 
 var proxy = httpProxy.createProxyServer({})
 
-http.createServer(function (req, res) {
-    console.log(req.headers);
-    res.headers['Access-Control-Allow-Origin'] = '*';
-    res.headers['Access-Control-Allow-Headers'] = 'accept, access-control-allow-origin, content-type';
-    res.headers['Access-Control-Allow-Method'] = 'POST, GET, OPTIONS, PUT, DELETE, HEAD';
-    proxy.web(req, res, { target: req.headers.x-target });
+http.createServer(function(req, res) {
+  res.setHeader('Access-Control-Allow-Method', 'POST, GET, OPTIONS, PUT, DELETE, HEAD');
+  res.setHeader('Access-Control-Allow-Headers', 'accept, access-control-allow-origin, content-type, x-target');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method == 'OPTIONS') {
+    res.end();
+  } else {
+    proxy.web(req, res, {
+      target: req.headers['x-target']
+    });
+  }
 }).listen(10000);
-
